7eb5bf7f169b3dae7ed2a5e3627c9d51350751b8
Alex Nekhaev <alexnekhaev@gmail.com>
2017-11-23 12:30:24 +0300

    Initial Commit

---
+++ Diffurs/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Diffurs
+//
+//  Created by Alexander Nekhaev on 23.11.2017.
+//  Copyright © 2017 Alexander Nekhaev. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
